8495bb55d7661aec89d41a4263355c24cb81aee5
Ruslanios22 <ilgamovich@gmail.com>
2023-08-10 11:58:42 +0300

    Added Custumized Project

---
+++ YandexWeather/YandexWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YandexWeather
+//
+//  Created by Руслан Шигапов on 10.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
