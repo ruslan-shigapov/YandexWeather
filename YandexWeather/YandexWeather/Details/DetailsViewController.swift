f4096ee90264bf3365de563e3db01536bba5427b
Ruslanios22 <ilgamovich@gmail.com>
2023-08-18 11:26:36 +0300

    Added Details VC

---
+++ YandexWeather/YandexWeather/Details/DetailsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailsViewController.swift
+//  YandexWeather
+//
+//  Created by Руслан Шигапов on 18.08.2023.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
